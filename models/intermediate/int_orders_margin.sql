d671c397e6b6def1cca8a366623a181f125474f4
Constance FRIES <friesconstance@gmail.com>
2024-02-06 14:49:37 +0000

    kk

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,4 +1,11 @@
 SELECT 
-*
+orders_id,
+date_date,
+ROUND(SUM(revenue),2) as revenue,
+SUM(quantity) as quantity,
+ROUND(SUM(purchase_cost),2) as purchase_cost,
+ROUND(SUM(margin),2) as margin,
 FROM 
 {{ ref('int_sales_margin')}}
+WHERE orders_id = 1002561
+GROUP BY orders_id, date_date
