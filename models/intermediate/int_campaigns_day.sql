83efba0cea42fdb1bf00ce157d3dfaab7db72045
Constance FRIES <friesconstance@gmail.com>
2024-02-07 14:55:54 +0000

    int_campaigns_day

--- models/intermediate/int_campaigns_day.sql
+++ models/intermediate/int_campaigns_day.sql
@@ -1,8 +1,9 @@
 SELECT 
   date_date,
   SUM(ads_cost) AS ads_cost,
-  (SUM(ads_cost) * impression) AS ads_impression,
+  SUM(impression) AS ads_impression,
   SUM(click) AS ads_clicks
 FROM {{ ref('int_campaigns') }}
+--WHERE date_date ILIKE 2021-07-31
 GROUP BY date_date
-ORDER BY date_date DESC;
+ORDER BY date_date DESC
