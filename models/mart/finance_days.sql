b51898efbb7fd77af692d13eb3369e01a854ad1d
Constance FRIES <friesconstance@gmail.com>
2024-02-07 10:03:44 +0000

    config finance_days as a table

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,3 +1,9 @@
+{{ config
+(materialized='table') 
+}}
+
+
+
 SELECT
     date_date
     ,COUNT(orders_id) AS nb_transactions
@@ -14,3 +20,4 @@ SELECT
 FROM {{ref("int_orders_operational")}}
 GROUP BY  date_date
 ORDER BY  date_date DESC
+
